178375e8d26cdc20797e63d457ea3b6a0b5da675
Nicole Uribe <nikyaleuribe@hotmail.com>
2022-11-17 20:33:32 -0400

    5 pruebas de unidad

--- src/telecomunicacio.test.js
+++ src/telecomunicacio.test.js
@@ -1,10 +1,33 @@
 import Facturacion from "./telecomunicacion.js";
 
 describe("telecomunicacion", () => {
+
+  
     const factura = new Facturacion();
+
   it("1. Tipo de pago", () => {
     expect(factura.pago("postpago")).toEqual(true);
     expect(factura.pago("prepago")).toEqual(false);
   });
 
+  it("2. Tipo de plan postpago", () => {
+    expect(factura.plan("postpago", 200)).toEqual("WAO");
+    expect(factura.plan("postpago", 150)).toEqual("L a medida");
+    expect(factura.plan("postpago", 50)).toEqual("S a medida");
+    expect(factura.plan("postpago", 100)).toEqual("M a medida");
+  });
+
+  it("3. Tipo de plan", () => {
+    expect(factura.plan("prepago", 150)).toEqual("prepago");
+  });
+
+  it("4. Identificar cuando no pueden tener addON", () => {
+    expect(factura.extras("postpago", 200)).toEqual(false);
+    expect(factura.extras("prepago", 130)).toEqual(false);
+  });
+
+  it("5. Identificar addON", () => {
+    expect(factura.extras("postpago", 50)).toEqual(true);
+    expect(factura.extras("postpago", 100)).toEqual(true);
+  });
 });
--- src/telecomunicacion.js
+++ src/telecomunicacion.js
@@ -1,12 +1,38 @@
 class Facturacion{
     constructor() {
     }
+    
     pago(tipo){
        if(tipo=="postpago")
        return true
        return false
 
     }
+
+    planPostpago(pago){
+        if(pago==200)
+        return "WAO"
+        if(pago==50)
+        return "S a medida"
+        if(pago==100)
+        return "M a medida"
+        if(pago==150)
+        return "L a medida"
+
+    }
+
+    plan(tipo, monto){ 
+        if(this.pago(tipo))
+        return this.planPostpago(monto)
+        return tipo
+    }
+
+    extras(tipo, monto){
+        if(this.plan(tipo,monto)!="prepago" && this.plan(tipo,monto)!="WAO")
+        return true
+        return false
+    }
+
     
 }
 export default Facturacion
